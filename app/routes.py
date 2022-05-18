345967b68c8860b0c20c11487b16f20ed6c35b61
Louis Boswell <ed193lb@leeds.ac.uk>
2022-05-18 14:10:13 +0100

    Spotipy being weird

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,16 @@ from flask import render_template
 from flask_sqlalchemy import model
 from app import app, db
 
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    
+    scope = "user-library-read"
+
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+
+    results = sp.current_user_saved_tracks()
+    
+    return render_template('index.html', results=results)
